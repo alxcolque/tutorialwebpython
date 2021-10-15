7d7e0ce778333bd673f2048a295b9e87b944e78c
unknown <a.colq.c@hotmail.com>
2021-10-14 23:48:04 -0400

    Working in controller  and view index

--- app/controllers/MainController.py
+++ app/controllers/MainController.py
@@ -1,9 +1,12 @@
+from app.models.User import User
+from app import db
 from flask import render_template
 class MainController():
     def __init__(self):
         pass
 
     def index(self):
-        user1 = {'name': 'Tupaj','surname':'Martinez'}
-        return render_template('index.html', user=user1)
+        #user = {'name': 'Tupaj','surname':'Martinez'}
+        users = User.query.all()
+        return render_template('index.html', users=users)
 maincontroller = MainController()
